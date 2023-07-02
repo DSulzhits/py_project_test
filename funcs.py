bfc8b1b667f5a549379ba210a4debb57408aed3f
DSulzhits <dsulzhits@gmail.com>
2023-07-02 14:58:04 +0300

    change functions summ and divide in funcs.py

--- funcs.py
+++ funcs.py
@@ -1,11 +1,13 @@
 def summ(a, b):
-    return a + b
+    c = a + b
+    return c
 
 
 # print(summ(3, 5))
 
 
 def divide(a, b):
-    return a - b
+    c = a - b
+    return c
 
 # print(divide(3, 5))
